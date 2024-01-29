d970aa1da1b04ce37a498775edd5137b4e6cd7f9
plokid <910576949@qq.com>
2024-01-29 20:46:25 +0800

    remove vscode settings

--- users/ploki/editors/vscode/default.nix
+++ users/ploki/editors/vscode/default.nix
@@ -55,121 +55,121 @@
           sha256 = "SPcSnS7LnRL5gdiJIVsFaN7eccrUHSj9uQYIQZllm0M=";
         }
       ];
-    userSettings = {
-      "editor.fontSize" = 16;
-      "editor.fontFamily" = "'Fira Code', 'monospace', monospace, 'MesloLGS NF'";
-      "window.zoomLevel" = 1.5;
-      "terminal.integrated.fontSize" = 16;
-      "files.autoSave" = "afterDelay";
-      "hediet.vscode-drawio.resizeImage" = "null";
-      "explorer.confirmDragAndDrop" = false;
-      "multiCommand.commands" = [
-        {
-          "command" = "multiCommand.multiEscape";
-          "sequence" = [
-            "workbench.action.focusFirstEditorGroup"
-            "extension.vim_escape"
-            "closeFindWidget"
-            "hideSuggestWidget"
-            "notifications.clearAll"
-          ];
-        }
-      ];
-      "vim.leader" = " ";
-      "vim.easymotion" = true;
-      "vim.easymotionKeys" = "hklyuiopnm,qwertzxcvbasdgjf;";
-      "vim.easymotionJumpToAnywhereRegex" = "\\b[A-Za-z0-9]|[A-Za-z0-9]\\b|_.|#.|[a-z][A-Z]";
-      "vim.highlightedyank.color" = "#343434";
-      "vim.highlightedyank.enable" = true;
-      "vim.highlightedyank.textColor" = "#ffffff";
-      "vim.scroll" = 0;
-      "vim.showcmd" = true;
-      "vim.surround" = true;
-      "vim.useSystemClipboard" = true;
-      "vim.vimrc.enable" = false;
-      "vim.normalModeKeyBindingsNonRecursive" = [
-        {
-          # toggle easy motion, but two word
-          "before" = [ "s" ];
-          "after" = [ "leader" "leader" "2" "s" ];
-        }
-        {
-          # toggle easy motion
-          "before" = [ "f" ];
-          "after" = [ "leader" "leader" "s" ];
-        }
-        {
-          # get info
-          "before" = [ "leader" "i" ];
-          "commands" = [ "editor.action.showHover" ];
-        }
-        {
-          # old vim `gi` command
-          "before" = [ "leader" "g" "i" ];
-          "after" = [ "g" "i" ];
-        }
-        {
-          # goto implementation
-          "before" = [ "g" "i" ];
-          "commands" = [ "editor.action.peekImplementation" ];
-        }
-        {
-          # goto error
-          "before" = [ "g" "e" ];
-          "commands" = [ "editor.action.marker.nextInFiles" ];
-        }
-        {
-          # goto word
-          "before" = [ "g" "w" ];
-          "after" = [ "y" "i" "w" "/" "<C-v>" "<CR>" ];
-        }
-        {
-          # git status/source
-          "before" = [ "leader" "g" "s" ];
-          "commands" = [ "extension.vim_escape" "workbench.view.scm" ];
-          "silent" = true;
-        }
-        {
-          # git log
-          "before" = [ "leader" "g" "l" ];
-          "commands" = [ "git-graph.view" ];
-        }
-        {
-          # git checkout
-          "before" = [ "leader" "g" "c" ];
-          "commands" = [ "gitlens.gitCommands.switch" ];
-        }
-        {
-          # yank path (default relative)
-          "before" = [ "leader" "y" "p" ];
-          "commands" = [ "copy-relative-path-and-line-numbers.both" ];
-        }
-        {
-          # new J
-          "before" = [ "J" ];
-          "after" = [ "5" "j" ];
-        }
-        {
-          # new K
-          "before" = [ "K" ];
-          "after" = [ "5" "k" ];
-        }
-        {
-          # new H
-          "before" = [ "H" ];
-          "after" = [ "5" "h" ];
-        }
-        {
-          # new L
-          "before" = [ "L" ];
-          "after" = [ "5" "l" ];
-        }
-        {
-          # toggle Sidebar
-          "before" = [ "leader" "e" ];
-          "after" = [ "workbench.action.toggleSidebarVisibility" ];
-        }
-      ];
-    };
+    # userSettings = {
+    #   "editor.fontSize" = 16;
+    #   "editor.fontFamily" = "'Fira Code', 'monospace', monospace, 'MesloLGS NF'";
+    #   "window.zoomLevel" = 1.5;
+    #   "terminal.integrated.fontSize" = 16;
+    #   "files.autoSave" = "afterDelay";
+    #   "hediet.vscode-drawio.resizeImage" = "null";
+    #   "explorer.confirmDragAndDrop" = false;
+    #   "multiCommand.commands" = [
+    #     {
+    #       "command" = "multiCommand.multiEscape";
+    #       "sequence" = [
+    #         "workbench.action.focusFirstEditorGroup"
+    #         "extension.vim_escape"
+    #         "closeFindWidget"
+    #         "hideSuggestWidget"
+    #         "notifications.clearAll"
+    #       ];
+    #     }
+    #   ];
+    #   "vim.leader" = " ";
+    #   "vim.easymotion" = true;
+    #   "vim.easymotionKeys" = "hklyuiopnm,qwertzxcvbasdgjf;";
+    #   "vim.easymotionJumpToAnywhereRegex" = "\\b[A-Za-z0-9]|[A-Za-z0-9]\\b|_.|#.|[a-z][A-Z]";
+    #   "vim.highlightedyank.color" = "#343434";
+    #   "vim.highlightedyank.enable" = true;
+    #   "vim.highlightedyank.textColor" = "#ffffff";
+    #   "vim.scroll" = 0;
+    #   "vim.showcmd" = true;
+    #   "vim.surround" = true;
+    #   "vim.useSystemClipboard" = true;
+    #   "vim.vimrc.enable" = false;
+    #   "vim.normalModeKeyBindingsNonRecursive" = [
+    #     {
+    #       # toggle easy motion, but two word
+    #       "before" = [ "s" ];
+    #       "after" = [ "leader" "leader" "2" "s" ];
+    #     }
+    #     {
+    #       # toggle easy motion
+    #       "before" = [ "f" ];
+    #       "after" = [ "leader" "leader" "s" ];
+    #     }
+    #     {
+    #       # get info
+    #       "before" = [ "leader" "i" ];
+    #       "commands" = [ "editor.action.showHover" ];
+    #     }
+    #     {
+    #       # old vim `gi` command
+    #       "before" = [ "leader" "g" "i" ];
+    #       "after" = [ "g" "i" ];
+    #     }
+    #     {
+    #       # goto implementation
+    #       "before" = [ "g" "i" ];
+    #       "commands" = [ "editor.action.peekImplementation" ];
+    #     }
+    #     {
+    #       # goto error
+    #       "before" = [ "g" "e" ];
+    #       "commands" = [ "editor.action.marker.nextInFiles" ];
+    #     }
+    #     {
+    #       # goto word
+    #       "before" = [ "g" "w" ];
+    #       "after" = [ "y" "i" "w" "/" "<C-v>" "<CR>" ];
+    #     }
+    #     {
+    #       # git status/source
+    #       "before" = [ "leader" "g" "s" ];
+    #       "commands" = [ "extension.vim_escape" "workbench.view.scm" ];
+    #       "silent" = true;
+    #     }
+    #     {
+    #       # git log
+    #       "before" = [ "leader" "g" "l" ];
+    #       "commands" = [ "git-graph.view" ];
+    #     }
+    #     {
+    #       # git checkout
+    #       "before" = [ "leader" "g" "c" ];
+    #       "commands" = [ "gitlens.gitCommands.switch" ];
+    #     }
+    #     {
+    #       # yank path (default relative)
+    #       "before" = [ "leader" "y" "p" ];
+    #       "commands" = [ "copy-relative-path-and-line-numbers.both" ];
+    #     }
+    #     {
+    #       # new J
+    #       "before" = [ "J" ];
+    #       "after" = [ "5" "j" ];
+    #     }
+    #     {
+    #       # new K
+    #       "before" = [ "K" ];
+    #       "after" = [ "5" "k" ];
+    #     }
+    #     {
+    #       # new H
+    #       "before" = [ "H" ];
+    #       "after" = [ "5" "h" ];
+    #     }
+    #     {
+    #       # new L
+    #       "before" = [ "L" ];
+    #       "after" = [ "5" "l" ];
+    #     }
+    #     {
+    #       # toggle Sidebar
+    #       "before" = [ "leader" "e" ];
+    #       "after" = [ "workbench.action.toggleSidebarVisibility" ];
+    #     }
+    #   ];
+    # };
   };
 }
