810ed3333fd887f3b9c205cfa52cd345c0c6b99f
plokid <910576949@qq.com>
2024-02-29 17:58:20 +0800

    add texlive packages

--- users/ploki/devlop/texlive/default.nix
+++ users/ploki/devlop/texlive/default.nix
@@ -6,11 +6,27 @@
     extraPackages = tpkgs: {
       inherit (tpkgs)
         latexmk
+        latexindent
         collection-fontsrecommended
         collection-fontsextra
         collection-langchinese
         collection-mathscience
         algorithms
+        biblatex
+        biblatex-trad
+        biblatex-ieee
+        biblatex-gb7714-2015
+        biber
+        xltxtra
+        xstring
+        realscripts
+        xifthen
+        ifmtarg
+        titlesec
+        enumitem
+        nth
+        setspace
+        cite
         listings;
     };
   };
