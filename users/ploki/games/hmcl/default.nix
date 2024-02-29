1d3af9437e333c29a727414d27f46e830cd38e0c
plokid <910576949@qq.com>
2024-02-29 17:57:55 +0800

    fix openj9 jdk package

--- users/ploki/games/hmcl/default.nix
+++ users/ploki/games/hmcl/default.nix
@@ -1,10 +1,11 @@
-{config,pkgs,...}:
+{ config, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     hmcl
     adoptopenjdk-openj9-bin-8
-    adoptopenjdk-openj9-bin-11
-    adoptopenjdk-openj9-bin-17
+    # adoptopenjdk-openj9-bin-11
+    # adoptopenjdk-openj9-bin-17
   ];
 }
+
