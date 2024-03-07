6c040726b57d052f8a96b1bcaa97227ff488dbaa
plokid <910576949@qq.com>
2024-03-07 14:05:46 +0800

    use aerial

--- users/ploki/editors/nvim/lua/config/lazy.lua
+++ users/ploki/editors/nvim/lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.test.core" },
 		{ import = "lazyvim.plugins.extras.editor.leap" },
 		{ import = "lazyvim.plugins.extras.ui.edgy" },
-		{ import = "lazyvim.plugins.extras.editor.symbols-outline" },
+		{ import = "lazyvim.plugins.extras.editor.aerial" },
 		{ import = "lazyvim.plugins.extras.ui.mini-animate" },
 		{ import = "lazyvim.plugins.extras.util.project" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
