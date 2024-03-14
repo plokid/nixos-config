a89bceb6c34cf94ca2dc5a9f7f801db4da8312a5
plokid <910576949@qq.com>
2024-03-14 14:08:05 +0800

    add some ext for pass

--- users/ploki/programs/common/password-store/default.nix
+++ users/ploki/programs/common/password-store/default.nix
@@ -3,7 +3,13 @@
 {
   programs.password-store = {
     enable = true;
-    package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
+    package = pkgs.pass.withExtensions (exts: [
+      exts.pass-otp
+      exts.pass-tomb
+      exts.pass-import
+      exts.pass-update
+      exts.pass-audit
+    ]);
   };
 
   programs.browserpass = {
