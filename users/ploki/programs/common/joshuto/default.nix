aec96ffc83d8b3c631a69c2f6a68f68118fc2e30
plokid <910576949@qq.com>
2024-04-04 21:27:48 +0800

    joshuto config

--- users/ploki/programs/common/joshuto/default.nix
+++ users/ploki/programs/common/joshuto/default.nix
@@ -1,10 +1,23 @@
 { pkgs, config, ... }:
 {
-  home = {
-    packages = with pkgs; [
-      file
-      joshuto
-    ];
+  programs.joshuto = {
+    enable = true;
   };
+
+  home.packages = with pkgs; [
+    file
+    # pdftotext
+    exiftool
+    transmission
+    pandoc
+    odt2txt
+    xlsx2csv
+    w3m
+    mediainfo
+    catdoc
+    mu
+    # djvutxt
+  ];
+
   home.file.".config/joshuto".source = ./config;
 }
