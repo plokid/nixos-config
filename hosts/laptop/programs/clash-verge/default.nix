4eeb2f827a2190cec618746eff55ac786e9ce30c
plokid <910576949@qq.com>
2024-04-14 19:16:47 +0800

    clash-verge to clash-clash-rev

--- hosts/laptop/programs/clash-verge/default.nix
+++ hosts/laptop/programs/clash-verge/default.nix
@@ -1,12 +1,13 @@
-{
+{ pkgs, ... }: {
   programs.clash-verge = {
     enable = true;
+    package = pkgs.clash-verge-rev;
     tunMode = true;
     autoStart = true;
   };
 
   networking.proxy = {
-    default = "http://localhost:7890/";
+    default = "http://localhost:7897/";
     noProxy = "127.0.0.1,localhost,internal.domain";
   };
 }
