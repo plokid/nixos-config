fd11cc651f3882167c967b56297c6fe32114e28b
plokid <910576949@qq.com>
2024-04-19 19:12:56 +0800

    update flake update script

--- scripts/flake-update.sh
+++ scripts/flake-update.sh
@@ -4,31 +4,4 @@ function trap_ctrlc {
 }
 trap "trap_ctrlc" 2
 
-flake_input=(
-  disko
-  emacs-overlay
-  flake-parts
-  flake-root
-  home-manager
-  hypr-contrib
-  hyprland
-  hyprpicker
-  impermanence
-  mission-control
-  nixpkgs
-  nur
-  picom
-  sops-nix
-  lanzaboote
-  rust-overlay
-  nil
-  neovim-nightly-overlay
-  treefmt-nix
-  emanote
-  joshuto
-  nixd
-)
-for item in "${flake_input[@]}"; do
-  echo -e "\033[1;34m${item}: \033[0m"
-  nix flake lock --update-input "$item"
-done
+nix flake update
