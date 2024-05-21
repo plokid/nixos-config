d608cef757854f9f7481da645d92ad7fb544beb4
plokid <910576949@qq.com>
2024-05-21 15:36:26 +0800

    regreet setting and background

--- hosts/laptop/desktop/regreet/default.nix
+++ hosts/laptop/desktop/regreet/default.nix
@@ -1,5 +1,31 @@
 {
   programs.regreet = {
     enable = true;
+    settings = {
+      background = {
+        path = ./greeter.jpg;
+        fit = "Contain";
+      };
+      GTK = {
+        # Whether to use the dark theme
+        application_prefer_dark_theme = true;
+
+        # Cursor theme name
+        cursor_theme_name = "Adwaita";
+
+        # Font name and size
+        font_name = "Cantarell 16";
+
+        # Icon theme name
+        icon_theme_name = "Adwaita";
+
+        # GTK theme name
+        theme_name = "Adwaita";
+      };
+      commands = {
+        reboot = [ "systemctl" "reboot" ];
+        poweroff = [ "systemctl" "poweroff" ];
+      };
+    };
   };
 }
